79d5b1940b851220d0bf75ad8f53e9432d40be97
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-10-14 12:35:00 +0200

    feat: WIP experimenting with prefetching for pilot searching

--- src/pilots.rs
+++ src/pilots.rs
@@ -19,6 +19,9 @@ impl<P: Packed, F: Packed, Rm: Reduce, Rn: Reduce, Hx: Hasher, Hk: Hasher, const
         bucket: &[Hash],
         taken: &mut BitVec,
     ) -> Option<(u64, Hash)> {
+        if bucket.len() < 6 {
+            return self.find_pilot_ringbuf(kmax, bucket, taken);
+        }
         let addr = taken.as_raw_slice().as_ptr();
 
         const L: u64 = 16;
@@ -84,5 +87,77 @@ impl<P: Packed, F: Packed, Rm: Reduce, Rn: Reduce, Hx: Hasher, Hk: Hasher, const
         taken: &mut BitVec,
     ) -> Option<(u64, Hash)> {
         self.find_pilot(kmax, bucket, taken)
+    }
+
+    #[inline(always)]
+    pub fn find_pilot_ringbuf(
+        &self,
+        kmax: u64,
+        bucket: &[Hash],
+        taken: &mut BitVec,
+    ) -> Option<(u64, Hash)> {
+        // We use get_unchecked everywhere possible.
+        unsafe {
+            let l = bucket.len();
+
+            type KI = u16;
+            type JS = u16;
+            const L: usize = 8;
+
+            let mut kis = [0 as KI; L];
+            // the j'th element of the bucket was prefetched and is next to be checked.
+            let mut js = [0 as JS; L];
+            let mut next_ki = 0u64;
+
+            let addr = taken.as_raw_slice().as_ptr();
+            let prefetch = |hx: Hash, ki: KI| {
+                let p = self.position(hx, ki as u64);
+                prefetch_read_data(addr.add(p / usize::BITS as usize), 3);
+            };
+
+            // Fill cache and prefetch.
+            for i in 0..L {
+                kis[i] = next_ki as KI;
+                js[i] = 0;
+                prefetch(*bucket.get_unchecked(js[i] as usize), kis[i]);
+                next_ki += 1;
+            }
+
+            loop {
+                for i in 0..L {
+                    // Lookup element at position i.
+                    let p = self.position(*bucket.get_unchecked(js[i] as usize), kis[i] as u64);
+                    if *taken.get_unchecked(p) {
+                        // Move to next ki.
+                        kis[i] = next_ki as KI;
+                        js[i] = 0;
+                        next_ki += 1;
+                        if next_ki == kmax {
+                            return None;
+                        }
+                    } else {
+                        if js[i] as usize == l - 1 {
+                            // SUCCESS
+                            if self.try_take_ki(bucket, self.hash_ki(kis[i] as u64), taken) {
+                                return Some((kis[i] as u64, self.hash_ki(kis[i] as u64)));
+                            } else {
+                                // Move to next ki.
+                                kis[i] = next_ki as KI;
+                                js[i] = 0;
+                                next_ki += 1;
+                                if next_ki == kmax {
+                                    return None;
+                                }
+                            }
+                        }
+                        js[i] += 1;
+                    }
+
+                    // Set next element to prefetch.
+                    // Prefetch.
+                    prefetch(*bucket.get_unchecked(js[i] as usize), kis[i]);
+                }
+            }
+        }
     }
 }
