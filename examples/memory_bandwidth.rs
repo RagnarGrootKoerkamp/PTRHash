a25b53ddcb98c9b9282d121bb9d2d73f3bce7037
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-11-08 19:18:58 +0100

    example: update memory bandwidth experiment for multithreading

--- examples/memory_bandwidth.rs
+++ examples/memory_bandwidth.rs
@@ -6,7 +6,7 @@ use rand::{thread_rng, Rng};
 use rayon::prelude::*;
 
 const CACHELINE: usize = 64;
-const ITS: usize = 10;
+const ITS: usize = 20;
 
 fn main() {
     // Allocate 4GB bytes of random data.
@@ -21,10 +21,23 @@ fn main() {
     // test_full(&data);
     // test_cacheline(&data);
     // test_stride(&data);
-    for stride in [1, 2, 4, 8, 16, 32, 64, 128] {
-        eprintln!("stride {}", stride);
-        test_stride(&data, stride);
-        test_prefetch(&data, stride);
+    let strides = [103, 29, 53, 13, 193, 149];
+    for threads in [1, 2, 3, 4, 5, 6] {
+        eprint!("Threads: {}", threads);
+        let start = Instant::now();
+        rayon::scope(|scope| {
+            for tid in 0..threads {
+                // let data = data.clone();
+                let data = &data;
+                scope.spawn(move |_| test_stride::<true>(data, strides[tid]));
+            }
+        });
+        let e = start.elapsed();
+        eprintln!(
+            "  {:>8.2?} {:>10.3}GB/s",
+            e,
+            (ITS * threads * n) as f64 / e.as_nanos() as f64
+        );
     }
 }
 
@@ -69,40 +82,20 @@ fn test_cacheline(data: &Vec<u8>) {
 }
 
 // #[inline(never)]
-fn test_stride(data: &Vec<u8>, s: usize) {
-    let n = data.len();
-    let start = Instant::now();
-    let mut sum2 = 0;
-    let s = s * CACHELINE;
-    for _ in 0..ITS {
-        for offset in (0..s).step_by(CACHELINE) {
-            for i in (offset..n).step_by(s) {
-                unsafe {
-                    sum2 += *data.get_unchecked(i) as u64;
-                }
-            }
-        }
-    }
-    let e = start.elapsed();
-    black_box(sum2);
-    eprintln!(
-        "strided    {:>8.2?} {:>10.3}GB/s",
-        e,
-        (ITS * n) as f64 / e.as_nanos() as f64
-    );
-}
+fn test_stride<const PREFETCH: bool>(data: &Vec<u8>, s: usize) {
+    let skip = 11;
 
-// #[inline(never)]
-fn test_prefetch(data: &Vec<u8>, s: usize) {
     let n = data.len();
     let start = Instant::now();
     let mut sum2 = 0;
-    let s = s * CACHELINE;
+    let cs = s * CACHELINE;
     for _ in 0..ITS {
-        for offset in (0..s).step_by(CACHELINE) {
-            for i in (offset..n).step_by(s) {
+        for offset in 0..s {
+            for i in (offset * CACHELINE..n).step_by(cs) {
                 unsafe {
-                    prefetch_read_data(data.as_ptr().add(i + 10 * s), 3);
+                    if PREFETCH {
+                        prefetch_read_data(data.as_ptr().add(i + 10 * cs), 3);
+                    }
                     sum2 += *data.get_unchecked(i) as u64;
                 }
             }
@@ -110,9 +103,9 @@ fn test_prefetch(data: &Vec<u8>, s: usize) {
     }
     let e = start.elapsed();
     black_box(sum2);
-    eprintln!(
-        "prefetch   {:>8.2?} {:>10.3}GB/s",
-        e,
-        (ITS * n) as f64 / e.as_nanos() as f64
-    );
+    // eprintln!(
+    //     "strided    {:>8.2?} {:>10.3}GB/s",
+    //     e,
+    //     (ITS * n) as f64 / e.as_nanos() as f64
+    // );
 }
