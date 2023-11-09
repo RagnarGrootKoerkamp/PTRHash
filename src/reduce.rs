e27cad85be41c551c11321c2eaa7252a49f93d91
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-11-10 00:19:17 +0100

    cleanup: Move unused FastMod to separate file

--- src/reduce.rs
+++ src/reduce.rs
@@ -26,56 +26,6 @@ impl Reduce for u64 {
     }
 }
 
-// Multiply a u128 by u64 and return the upper 64 bits of the result.
-// ((lowbits * d as u128) >> 128) as u64
-fn mul128_u64(lowbits: u128, d: u64) -> u64 {
-    let bot_half = ((lowbits & u64::MAX as u128) * d as u128) >> 64; // Won't overflow
-    let top_half = (lowbits >> 64) * d as u128;
-    let both_halves = bot_half + top_half; // Both halves are already shifted down by 64
-    (both_halves >> 64) as u64
-}
-
-/// FastMod64
-/// Taken from https://github.com/lemire/fastmod/blob/master/include/fastmod.h
-#[derive(Copy, Clone, Debug)]
-pub struct FM64 {
-    d: u64,
-    m: u128,
-}
-impl Reduce for FM64 {
-    fn new(d: usize) -> Self {
-        Self {
-            d: d as u64,
-            m: u128::MAX / d as u128 + 1,
-        }
-    }
-    fn reduce(self, h: u64) -> usize {
-        let lowbits = self.m.wrapping_mul(h as u128);
-        mul128_u64(lowbits, self.d) as usize
-    }
-}
-
-/// FastMod32, using the low 32 bits of the hash.
-/// Taken from https://github.com/lemire/fastmod/blob/master/include/fastmod.h
-#[derive(Copy, Clone, Debug)]
-pub struct FM32 {
-    d: u64,
-    m: u64,
-}
-impl Reduce for FM32 {
-    fn new(d: usize) -> Self {
-        assert!(d <= u32::MAX as usize);
-        Self {
-            d: d as u64,
-            m: u64::MAX / d as u64 + 1,
-        }
-    }
-    fn reduce(self, h: u64) -> usize {
-        let lowbits = self.m * (h as u64);
-        ((lowbits as u128 * self.d as u128) >> 64) as usize
-    }
-}
-
 /// FastReduce64
 /// Taken from https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
 /// NOTE: This only uses the lg(n) high-order bits of entropy from the hash.
