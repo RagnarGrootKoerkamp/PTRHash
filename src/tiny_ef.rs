508a9be2d4e8902b149ab5a8b328bda78a5b1035
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-11-13 00:32:41 +0100

    bugfix: Reorder TinyEf fields to have 64bit size under repr(C)
    Reinstated align(64) with ε-serde 0.2.2
    
    Reordered fields to reduce size

--- src/tiny_ef.rs
+++ src/tiny_ef.rs
@@ -53,12 +53,12 @@ impl<E: AsRef<[TinyEfUnit]>> TinyEf<E> {
 #[repr(align(64))]
 #[zero_copy]
 pub struct TinyEfUnit {
-    // The offset of the first element, divided by 256.
-    reduced_offset: u32,
     // 2*64 = 128 bits to indicate where 256 boundaries are crossed.
     // There are 48 1-bits corresponding to the stored numbers, and the number
     // of 0-bits before each number indicates the number of times 256 must be added.
     high_boundaries: [u64; 2],
+    // The offset of the first element, divided by 256.
+    reduced_offset: u32,
     // Last 8 bits of each number.
     low_bits: [u8; L],
 }
