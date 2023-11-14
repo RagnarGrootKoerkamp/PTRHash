65882d84fd407dee1a65a394b66c48ba8740efc9
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-11-14 19:29:35 +0100

    feat: Implement and compare hash functions

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -71,6 +71,20 @@ enum Command {
     },
 }
 
+// Fast hashes
+// type PH = PtrHash<TinyEf, hash::FxHash, Vec<u8>>;
+// type PH = PtrHash<TinyEf, hash::Murmur2_64, Vec<u8>>;
+type PH = PtrHash<TinyEf, hash::Murmur3_128, Vec<u8>>;
+// type PH = PtrHash<TinyEf, hash::FastMurmur3_128, Vec<u8>>;
+
+// Slow hashes
+// type PH = PtrHash<TinyEf, hash::Highway64, Vec<u8>>;
+// type PH = PtrHash<TinyEf, hash::City64, Vec<u8>>;
+// type PH = PtrHash<TinyEf, hash::Xx128, Vec<u8>>;
+// type PH = PtrHash<TinyEf, hash::Metro64, Vec<u8>>;
+// type PH = PtrHash<TinyEf, hash::Spooky64, Vec<u8>>;
+// type PH = PtrHash<TinyEf, hash::Spooky128, Vec<u8>>;
+
 fn main() -> anyhow::Result<()> {
     let Args { command } = Args::parse();
 
@@ -90,7 +104,7 @@ fn main() -> anyhow::Result<()> {
                 .build_global()
                 .unwrap();
             let keys = ptr_hash::util::generate_keys(n);
-            let pt = <PtrHash>::new(
+            let pt = PH::new(
                 &keys,
                 PtrHashParams {
                     c,
@@ -125,27 +139,69 @@ fn main() -> anyhow::Result<()> {
                 .unwrap();
             let keys = ptr_hash::util::generate_keys(n);
             #[cfg(feature = "epserde")]
-            let pt = <PtrHash>::mmap("pt.bin", Flags::default())?;
+            let pt = PH::mmap("pt.bin", Flags::default())?;
             #[cfg(not(feature = "epserde"))]
-            let pt = PtrHash::new_random(
-                n,
-                PtrHashParams {
-                    c,
-                    alpha,
-                    print_stats: stats,
-                    slots_per_part: s,
-                    keys_per_shard,
-                    shard_to_disk: disk,
-                    ..Default::default()
-                },
-            );
-
             let loops = total.div_ceil(n);
 
-            let query = bench_index(loops, &keys, |key| pt.index(key));
-            eprintln!(" (1): {query:>4.1}");
-            let query = bench_index(loops, &keys, |key| pt.index_minimal(key));
-            eprintln!(" (1): {query:>4.1}");
+            fn test<H: Hasher>(loops: usize, keys: &[u64], params: &PtrHashParams) {
+                type PH<H> = PtrHash<TinyEf, H, Vec<u8>>;
+                let pt = PH::<H>::new_random(keys.len(), *params);
+
+                let query = bench_index(loops, keys, |key| pt.index(key));
+                eprintln!("  sequential: {query:>4.1}");
+                let query = time(loops, keys, || {
+                    pt.index_stream::<64, false>(keys).sum::<usize>()
+                });
+                eprintln!(" prefetch 64: {query:>5.2}ns");
+            }
+
+            let params = PtrHashParams {
+                c,
+                alpha,
+                print_stats: stats,
+                slots_per_part: s,
+                keys_per_shard,
+                shard_to_disk: disk,
+                ..Default::default()
+            };
+            eprintln!("fxhash");
+            test::<hash::FxHash>(loops, &keys, &params);
+            eprintln!("murmur2");
+            test::<hash::Murmur2_64>(loops, &keys, &params);
+            eprintln!("fastmurmur3");
+            test::<hash::FastMurmur3_128>(loops, &keys, &params);
+            eprintln!("murmur3");
+            test::<hash::Murmur3_128>(loops, &keys, &params);
+
+            eprintln!("highway64");
+            test::<hash::Highway64>(loops, &keys, &params);
+            eprintln!("highway128");
+            test::<hash::Highway128>(loops, &keys, &params);
+            eprintln!("city64");
+            test::<hash::City64>(loops, &keys, &params);
+            eprintln!("city128");
+            test::<hash::City128>(loops, &keys, &params);
+            eprintln!("xx64");
+            test::<hash::Xx64>(loops, &keys, &params);
+            eprintln!("xx128");
+            test::<hash::Xx128>(loops, &keys, &params);
+            eprintln!("metro64");
+            test::<hash::Metro64>(loops, &keys, &params);
+            eprintln!("metro128");
+            test::<hash::Metro128>(loops, &keys, &params);
+            eprintln!("spooky64");
+            test::<hash::Spooky64>(loops, &keys, &params);
+            eprintln!("spooky128");
+            test::<hash::Spooky128>(loops, &keys, &params);
+
+            // let query = bench_index(loops, &keys, |key| pt.index(key));
+            // eprintln!(" ( 1  )  : {query:>4.1}");
+            // let query = time(loops, &keys, || {
+            //     index_parallel::<64, _, _, _>(&pt, &keys, threads, false)
+            // });
+            // eprintln!(" (64t{threads})  : {query:>5.2}ns");
+            // let query = bench_index(loops, &keys, |key| pt.index_minimal(key));
+            // eprintln!(" ( 1  )+r: {query:>4.1}");
 
             // let query = bench_index_all(loops, &keys, |keys| pt.index_stream::<32>(keys));
             // eprint!(" (32): {query:>4.1}");
@@ -153,16 +209,18 @@ fn main() -> anyhow::Result<()> {
             // eprintln!(" (1): {query:>4.1}");
             // let query = time(loops, &keys, || pt.index_stream::<64>(&keys).sum());
             // eprint!(" (64): {query:>4.1}");
-            for threads in 1..=6 {
-                let query = time(loops, &keys, || {
-                    index_parallel::<64, _, _, _>(&pt, &keys, threads, false)
-                });
-                eprintln!(" (64t{threads})  : {query:>5.2}ns");
-                let query = time(loops, &keys, || {
-                    index_parallel::<64, _, _, _>(&pt, &keys, threads, true)
-                });
-                eprintln!(" (64t{threads})+r: {query:>5.2}ns");
-            }
+
+            // for threads in 1..=6 {
+            //     let query = time(loops, &keys, || {
+            //         index_parallel::<64, _, _, _>(&pt, &keys, threads, false)
+            //     });
+            //     eprintln!(" (64t{threads})  : {query:>5.2}ns");
+            //     let query = time(loops, &keys, || {
+            //         index_parallel::<64, _, _, _>(&pt, &keys, threads, true)
+            //     });
+            //     eprintln!(" (64t{threads})+r: {query:>5.2}ns");
+            // }
+
             // let query = bench_index_all(loops, &keys, |keys| pt.index_stream::<128>(keys));
             // eprint!(" (128): {query:>4.1}");
 
--- src/hash.rs
+++ src/hash.rs
@@ -1,6 +1,6 @@
-use std::fmt::Debug;
-
 use crate::Key;
+use highway::HighwayHash;
+use std::fmt::Debug;
 
 /// A wrapper trait that supports both 64 and 128bit hashes.
 pub trait Hash: Copy + Debug + Default + Send + Sync + Eq + rdst::RadixKey {
@@ -33,18 +33,32 @@ pub trait Hasher: Sync {
     fn hash(x: &Key, seed: u64) -> Self::H;
 }
 
-pub struct Murmur;
+pub struct Murmur2_64;
 
-impl Hasher for Murmur {
+impl Hasher for Murmur2_64 {
     type H = u64;
     fn hash(x: &Key, seed: u64) -> u64 {
-        murmur2::murmur64a(
-            // Pass the key as a byte slice.
-            unsafe {
-                std::slice::from_raw_parts(x as *const Key as *const u8, std::mem::size_of::<Key>())
-            },
-            seed,
-        )
+        murmur2::murmur64a(&x.to_ne_bytes(), seed)
+    }
+}
+
+pub struct Murmur3_128;
+
+impl Hasher for Murmur3_128 {
+    type H = u128;
+    fn hash(x: &Key, seed: u64) -> u128 {
+        let bytes = x.to_ne_bytes();
+        let mut slice = &bytes[..];
+        murmur3::murmur3_x64_128(&mut slice, seed as u32).unwrap()
+    }
+}
+
+pub struct FastMurmur3_128;
+
+impl Hasher for FastMurmur3_128 {
+    type H = u128;
+    fn hash(x: &Key, seed: u64) -> u128 {
+        fastmurmur3::murmur3_x64_128(&x.to_ne_bytes(), seed)
     }
 }
 
@@ -83,5 +97,120 @@ impl Hasher for FxHash {
     type H = u64;
     fn hash(x: &Key, _seed: u64) -> u64 {
         fxhash::hash64(x)
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Highway64;
+
+impl Hasher for Highway64 {
+    type H = u64;
+    fn hash(x: &Key, _seed: u64) -> u64 {
+        highway::HighwayHasher::default().hash64(&x.to_ne_bytes())
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Highway128;
+
+impl Hasher for Highway128 {
+    type H = u128;
+    fn hash(x: &Key, _seed: u64) -> u128 {
+        let words = highway::HighwayHasher::default().hash128(&x.to_ne_bytes());
+        unsafe { std::mem::transmute(words) }
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct City64;
+
+impl Hasher for City64 {
+    type H = u64;
+    fn hash(x: &Key, _seed: u64) -> u64 {
+        cityhash_102_rs::city_hash_64(&x.to_ne_bytes())
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct City128;
+
+impl Hasher for City128 {
+    type H = u128;
+    fn hash(x: &Key, seed: u64) -> u128 {
+        cityhash_102_rs::city_hash_128_seed(&x.to_ne_bytes(), seed as _)
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Xx64;
+
+impl Hasher for Xx64 {
+    type H = u64;
+    fn hash(x: &Key, seed: u64) -> u64 {
+        xxhash_rust::xxh64::xxh64(&x.to_ne_bytes(), seed)
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Xx128;
+
+impl Hasher for Xx128 {
+    type H = u128;
+    fn hash(x: &Key, seed: u64) -> u128 {
+        xxhash_rust::xxh3::xxh3_128_with_seed(&x.to_ne_bytes(), seed)
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Metro64;
+
+impl Hasher for Metro64 {
+    type H = u64;
+    fn hash(x: &Key, seed: u64) -> u64 {
+        use std::hash::Hasher;
+        let mut hasher = metrohash::MetroHash64::with_seed(seed);
+        hasher.write(&x.to_ne_bytes());
+        hasher.finish()
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Metro128;
+
+impl Hasher for Metro128 {
+    type H = u128;
+    fn hash(x: &Key, seed: u64) -> u128 {
+        use std::hash::Hasher;
+        let mut hasher = metrohash::MetroHash128::with_seed(seed);
+        hasher.write(&x.to_ne_bytes());
+        let (l, h) = hasher.finish128();
+        (h as u128) << 64 | l as u128
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Spooky64;
+
+impl Hasher for Spooky64 {
+    type H = u64;
+    fn hash(x: &Key, seed: u64) -> u64 {
+        use std::hash::Hasher;
+        let mut hasher = hashers::jenkins::spooky_hash::SpookyHasher::new(seed, 0);
+        hasher.write(&x.to_ne_bytes());
+        hasher.finish()
+    }
+}
+
+#[cfg_attr(feature = "epserde", derive(epserde::prelude::Epserde))]
+pub struct Spooky128;
+
+impl Hasher for Spooky128 {
+    type H = u128;
+    fn hash(x: &Key, seed: u64) -> u128 {
+        use std::hash::Hasher;
+        let mut hasher = hashers::jenkins::spooky_hash::SpookyHasher::new(seed, 0);
+        hasher.write(&x.to_ne_bytes());
+        let (l, h) = hasher.finish128();
+        (h as u128) << 64 | l as u128
     }
 }
