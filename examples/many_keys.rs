7aae71207c697fb1a5d80d40551068fcbf06a3a6
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-11-15 20:01:37 +0100

    example: change to c=10 and use a different hash function

--- examples/many_keys.rs
+++ examples/many_keys.rs
@@ -3,7 +3,7 @@
 //!
 //! NOTE: This requires somewhere between 32 and 64GB of memory.
 use bitvec::bitvec;
-use ptr_hash::{PtrHash, PtrHashParams};
+use ptr_hash::{hash::*, tiny_ef::TinyEf, PtrHash, PtrHashParams};
 use rayon::prelude::*;
 
 fn main() {
@@ -11,10 +11,11 @@ fn main() {
     let n_query = 1 << 27;
     let range = 0..n as u64;
     let keys = range.clone().into_par_iter();
-    let ptr_hash = <PtrHash>::new_from_par_iter(
+    let ptr_hash = PtrHash::<_, TinyEf, Murmur2_64, _>::new_from_par_iter(
         n,
         keys.clone(),
         PtrHashParams {
+            c: 10.,
             // ~10GB of keys per shard.
             keys_per_shard: 1 << 29,
             shard_to_disk: false,
