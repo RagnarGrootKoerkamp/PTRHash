12e4395ae4afdba132140d5b91d5feadde62f59c
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-08-27 14:42:59 +0200

    Add hidden PtrHash::max_index_no_remap

--- src/lib.rs
+++ src/lib.rs
@@ -751,6 +751,14 @@ impl<
         }
     }
 
+    /// `self.index_no_remap()` always returns below this bound.
+    /// Around `n/alpha ~ 1.01*n`.
+    #[doc(hidden)]
+    pub fn max_index_no_remap(&self) -> usize {
+        eprintln!("Rem slots: {:?}", self.rem_slots);
+        self.slots_total
+    }
+
     pub fn slots_per_part(&self) -> usize {
         self.slots
     }
